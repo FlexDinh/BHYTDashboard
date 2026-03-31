7518691f6e61c82755ddf04a9c003307f3a78bf9
vuhoangtuanphuc <vuhoangtuanphuc@gmail.com>
2026-03-31 09:14:41 +0700

    Update database va tao SeedData

---
+++ Models/Entities/DanhMucNhomChiPhi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BHYTDashboard.Models.Entities;
+
+public partial class DanhMucNhomChiPhi
+{
+    public string MaNhomChiPhi { get; set; } = null!;
+
+    public string TenNhomChiPhi { get; set; } = null!;
+
+    public virtual ICollection<ChiTietDvkt> ChiTietDvkts { get; set; } = new List<ChiTietDvkt>();
+
+    public virtual ICollection<ChiTietThuoc> ChiTietThuocs { get; set; } = new List<ChiTietThuoc>();
+}
