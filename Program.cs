81f24b124137df90a7cac049354060fff7832e29
DESKTOP-KU34JC9 <thongminhtranle123@gmail.com>
2026-04-12 15:40:02 +0700

    update cho phép chọn năm và hiển thị năm hiện tại

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
-using BHYTDashboard.Services;
-using BHYTDashboard.Services.Interfaces;
+using BHYTDashboard.Data;
 using BHYTDashboard.Repositories;
 using BHYTDashboard.Repositories.Interfaces;
+using BHYTDashboard.Scripts;
+using BHYTDashboard.Services;
+using BHYTDashboard.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using OfficeOpenXml;
 
 // ===== KHỞI TẠO BUILDER =====
@@ -17,22 +20,18 @@ builder.Services.AddScoped<IDashboardService, DashboardService>();
 builder.Services.AddScoped<IExportExcelService, ExportExcelService>();
 // ----- Entity Framework Core -----
 // TODO (TV2): Bỏ comment khi đã tạo AppDbContext
-//builder.Services.AddDbContext<AppDbContext>(options =>
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<BenhVienUbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// ----- Dependency Injection -----
-// TODO (TV3): Bỏ comment khi đã tạo DashboardRepository
-// builder.Services.AddScoped<IDashboardRepository, DashboardRepository>();
 
-// TODO (TV3/TV5): Bỏ comment khi đã tạo DashboardService
-// builder.Services.AddScoped<IDashboardService, DashboardService>();
-
-// TODO (TV5): Bỏ comment khi đã tạo ExportExcelService
-// builder.Services.AddScoped<IExportExcelService, ExportExcelService>();
 
 // ===== BUILD APP =====
 var app = builder.Build();
-
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<BenhVienUbContext>();
+    SeedData.Initialize(context);
+}
 // ===== MIDDLEWARE PIPELINE =====
 if (!app.Environment.IsDevelopment())
 {
